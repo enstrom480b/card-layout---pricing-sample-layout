9b5fbb5134d02f1f070fbf3c11e7b17acb1ad314
enstrom480b <64636784+enstrom480b@users.noreply.github.com>
2021-02-19 15:55:37 -0600

    Add files via upload
    pricing-sample-table-design-html&css

---
+++ pricing-sample-9.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Demo CSS Table Price Hosting Plan</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7; IE=EmulateIE9">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+    <link rel="shortcut icon" href=http://www.freshdesignweb.com/wp-content/themes/fv24/images/icon.ico />
+  
+ <link rel="stylesheet" type="text/css" href="demo.css" media="all" />
+  <link rel="stylesheet" type="text/css" href="styles.css" media="all" />
+</head>
+<body>
+<div class="container">
+			<!-- freshdesignweb top bar -->
+            <div class="freshdesignweb-top">
+                <a href="http://www.freshdesignweb.com" target="_blank">Home</a>
+                <span class="right">
+                    <a href="http://www.freshdesignweb.com/free-beautiful-css3-table-style.html">
+                        <strong>Back to the freshdesignweb Article</strong>
+                    </a>
+                </span>
+                <div class="clr"></div>
+            </div><!--/ freshdesignweb top bar -->
+			<header>
+				<h1><span>Tutorials</span>Demo CSS Table Price Hosting Plan</h1>
+            </header>       
+     <!-- start header here-->
+	<header>
+<div id="fdw-pricing-table">
+    <div class="plan plan1">
+        <div class="header">Enterprise</div>
+        <div class="price">$59</div>  
+        <div class="monthly">per month</div>      
+        <ul>
+            <li><b>10GB</b> Disk Space</li>
+            <li><b>100GB</b> Monthly Bandwidth</li>
+            <li><b>20</b> Email Accounts</li>
+			<li><b>Unlimited</b> subdomains</li>			
+        </ul>
+        <a class="signup" href="http://www.freshdesignweb.com">Sign up</a>         
+    </div>
+    <div class="plan plan2 popular-plan">
+        <div class="header">Professional</div>
+        <div class="price">$29</div>
+        <div class="monthly">per month</div>  
+        <ul>
+            <li><b>5GB</b> Disk Space</li>
+            <li><b>50GB</b> Monthly Bandwidth</li>
+            <li><b>10</b> Email Accounts</li>
+			<li><b>Unlimited</b> subdomains</li>			
+        </ul>
+        <a class="signup" href="http://www.freshdesignweb.com">Sign up</a>            
+    </div>
+    <div class="plan plan3">
+        <div class="header">Standard</div>
+        <div class="price">$19</div>
+        <div class="monthly">per month</div>
+        <ul>
+            <li><b>3GB</b> Disk Space</li>
+            <li><b>25GB</b> Monthly Bandwidth</li>
+            <li><b>5</b> Email Accounts</li>
+			<li><b>Unlimited</b> subdomains</li>			
+        </ul>
+        <a class="signup" href="http://www.freshdesignweb.com">Sign up</a>        
+    </div>
+    <div class="plan plan4">
+        <div class="header">Basic</div>
+        <div class="price">$9</div>
+        <div class="monthly">per month</div>
+        <ul>
+            <li><b>1GB</b> Disk Space</li>
+            <li><b>10GB</b> Monthly Bandwidth</li>
+            <li><b>2</b> Email Accounts</li>
+			<li><b>Unlimited</b> subdomains</li>			
+        </ul>
+        <a class="signup" href="http://www.freshdesignweb.com">Sign up</a>        
+    </div> 	
+</div>
+	</header><!-- end header -->
+    
+</div>
+</body>
+</html>
+<style>
+/* CSS reset */
+body,div,dl,dt,dd,ul,ol,li,h1,h2,h3,h4,h5,h6,pre,form,fieldset,input,textarea,p,blockquote,th,td { 
+	margin:0;
+	padding:0;
+}
+html,body {
+	margin:0;
+	padding:0;
+	height: 100%;
+}
+table {
+	border-collapse:collapse;
+	border-spacing:0;
+}
+fieldset,img { 
+	border:0;
+}
+address,caption,cite,code,dfn,th,var {
+	font-style:normal;
+	font-weight:normal;
+}
+ol,ul {
+	list-style:none;
+}
+caption,th {
+	text-align:left;
+}
+h1,h2,h3,h4,h5,h6 {
+	font-size:100%;
+	font-weight:normal;
+}
+q:before,q:after {
+	content:'';
+}
+abbr,acronym { border:0;
+}
+section, header{
+	display: block;
+}
+/* General Demo Style */
+body{
+	font-family: Cambria, Palatino, "Palatino Linotype", "Palatino LT STD", Georgia, serif;
+	background:url(images/bgnoise_lg.png);
+	font-weight: 400;
+	font-size: 15px;
+	color: #3a2127;
+	overflow-y: scroll;
+}
+a{
+	color: #333;
+	text-decoration: none;
+}
+.container{
+	width: 100%;
+	height: 100%;
+	position: relative;
+}
+.clr{
+	clear: both;
+}
+.container > header{
+	padding: 20px 30px 20px 30px;
+	margin: 0px 20px 10px 20px;
+	position: relative;
+	display: block;
+	text-shadow: 1px 1px 1px rgba(0,0,0,0.2);
+    text-align: center;
+}
+.container > header h1{
+	position: relative;
+	color: #498ea5;
+	font-weight: 700;
+	font-style: normal;
+	font-size: 30px;
+	padding: 0px 0px 5px 0px;
+	text-shadow: 0px 1px 1px rgba(255,255,255,0.8);
+}
+.container > header h1 span{
+	font-family: 'Alegreya SC', Georgia, serif;
+	font-size: 20px;
+	line-height: 20px;
+	display: block;
+	font-weight: 400;
+	font-style: italic;
+	color: #719dab;
+	text-shadow: 1px 1px 1px rgba(0,0,0,0.1);
+}
+.container > header h2{
+	font-size: 16px;
+	font-style: italic;
+	color: #2d6277;
+	text-shadow: 0px 1px 1px rgba(255,255,255,0.8);
+}
+/* Header Style */
+.freshdesignweb-top{
+	line-height: 24px;
+	font-size: 11px;
+	background: rgba(0, 0, 0, 0.05);
+	text-transform: uppercase;
+	z-index: 9999;
+	position: relative;
+	box-shadow: 1px 0px 2px rgba(0,0,0,0.2);
+}
+.freshdesignweb-top a{
+	padding: 0px 10px;
+	letter-spacing: 1px;
+	color: #333;
+	text-shadow: 0px 1px 1px #fff;
+	display: block;
+	float: left;
+}
+.freshdesignweb-top a:hover{
+	background: #fff;
+}
+.freshdesignweb-top span.right{
+	float: right;
+}
+.freshdesignweb-top span.right a{
+	float: left;
+	display: block;
+}
+.freshdesignweb-demos{
+    text-align:center;
+	display: block;
+	line-height: 30px;
+	padding: 20px 0px;
+}
+.freshdesignweb-demos a{
+    display: inline-block;
+	margin: 0px 4px;
+	padding: 0px 4px;
+	color: #fff;
+	line-height: 20px;	
+	font-style: italic;
+	font-size: 13px;
+	border-radius: 3px;
+	background: rgba(41,77,95,0.1);
+	-webkit-transition: all 0.2s linear;
+	-moz-transition: all 0.2s linear;
+	-o-transition: all 0.2s linear;
+	-ms-transition: all 0.2s linear;
+	transition: all 0.2s linear;
+}
+.freshdesignweb-demos a:hover{
+	background: rgba(41,77,95,0.3);
+}
+.freshdesignweb-demos a.current,
+.freshdesignweb-demos a.current:hover{
+	background: rgba(41,77,95,0.3);
+}
+
+</style>
